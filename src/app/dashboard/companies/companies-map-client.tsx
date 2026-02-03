f077b569defaddca716a53f4cab0468f17ae2c23
kbmillard <n2t58hhg6s@privaterelay.appleid.com>
2026-02-02 23:39:12 -0600

    Map: token from server, restore companies-map, remove client wrapper

--- src/app/dashboard/companies/companies-map-client.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const CompaniesMap = dynamic(() => import('./companies-map'), { ssr: false });
-
-export default function CompaniesMapClient() {
-  return <CompaniesMap />;
-}
